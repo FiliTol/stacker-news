5b9d130b61f0d6516316766b4f61e90b4e93535f
FiliTol <tolinfilippo@gmail.com>
2023-10-10 15:46:56 +0200

    Minor changes

--- scripts/discussion.py
+++ scripts/discussion.py
@@ -5,25 +5,61 @@ from datetime import datetime
 from scripts import item
 
 
+NA = None
+
+
 # Function that scrapes a 'discussion' item
 def scrape_discussion():
-
     # Produces a string then inserted in a csv
     pass
 
 
-# Function that
-def extract_title():
-    pass
+# Function that extracts the item title from the provided page
+def extract_title(page):
+    # Produces a string then inserted in a csv
+    try:
+        title = page.find('a', class_='item_title__FH7AS text-reset me-2').get_text()
+    except:
+        title = NA
+    return title
 
 
-def extract_banner():
-    pass
+# Function that extracts the item banner from the provided page
+def extract_banner(page):
+    # Produces a string then inserted in a csv
+    try:
+        banner = page.find('div', class_='item_other__MjgP3')
+    except:
+        banner = NA
+    partial_banner_data = [i.text for i in banner.find_all('span')]
+
+    final_banner = {'sats': '',
+                    'boost': '',
+                    'comments': '',
+                    'author': '',
+                    'tag': '',
+                    'timestamp': '',
+                    }
+
+    for b in partial_banner_data:
+        if "boost" in b:
+            pass
+        if "sats" in b:
+            pass
+        if "@" in b:
+            pass
+
+    return  banner_data
 
 
+# Function that extracts the item body from the provided page
 def extract_body():
+    # Produces a string then inserted in a csv
     pass
 
 
+# Function that extracts the comments data
+# Data include both the `total sat collected by comments` and the `comment item number`
 def extract_comment_data():
+    # Produces a string then inserted in a csv
     pass
