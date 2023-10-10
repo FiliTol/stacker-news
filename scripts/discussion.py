f9e2d3cebbb718561768cbc92cd4f4c433e881b0
FiliTol <tolinfilippo@gmail.com>
2023-10-10 14:56:28 +0200

    Deleted redundancies

---
+++ scripts/discussion.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import requests as requests
+import re
+from datetime import datetime
+from scripts import item
+
+
+# Function that scrapes a 'discussion' item
+def scrape_discussion():
+
+    # Produces a string then inserted in a csv
+    pass
+
+
+# Function that
+def extract_title():
+    pass
+
+
+def extract_banner():
+    pass
+
+
+def extract_body():
+    pass
+
+
+def extract_comment_data():
+    pass
