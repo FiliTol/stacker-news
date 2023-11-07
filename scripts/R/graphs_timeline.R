c7e2385134edf3198489731d861e85e48d45bdb6
FiliTol <tolinfilippo@gmail.com>
2023-11-07 16:53:30 +0100

    Edit graphs: lowered alpha of 'other' nodes and saved images

--- scripts/R/graphs_timeline.R
+++ scripts/R/graphs_timeline.R
@@ -4,6 +4,7 @@ library(ggplot2)
 library(stringr)
 library(lubridate)
 library(tidyverse)
+library(RColorBrewer)
 
 #' # Load RDS files
 comments <- readRDS(file = 'RDS_files/comments')
@@ -21,157 +22,235 @@ c_second_period <- readRDS(file = 'RDS_files/c_second_period')
 c_third_period <- readRDS(file = 'RDS_files/c_third_period')
 c_fourth_period <- readRDS(file = 'RDS_files/c_fourth_period')
 c_fifth_period <- readRDS(file = 'RDS_files/c_fifth_period')
+  
+contrib <- c("k00b",
+               "kr",
+               "ekzyis",
+               "WeAreAllSatoshi",
+               "rleed",
+               "bitcoinplebdev",
+               "benthecarman"
+  )
 
 #' Network Analysis
 #' 
 #' ## First Period
-## ---------------------------------------------------------------------------------------------------------------------------------
-first_graph <- copy(p_first_period)
-first_graph <- first_graph[, .(Commentors = unlist(tstrsplit(CommentsItemCode, ", "))), by = "ItemCode"]
-first_graph <- merge(first_graph, posts, by.x = 'ItemCode', by.y = 'ItemCode')
-first_graph <- first_graph[, .(PostItemCode = ItemCode, CommentItemCode = Commentors, PostAuthor = Author)]
-first_graph <- merge(first_graph, comments, by.x = 'CommentItemCode', by.y = 'ItemCode')
-first_graph <- first_graph[, .(PostItemCode, CommentItemCode, PostAuthor, CommentAuthor = Author)]
-first_graph <- first_graph[, .(PostAuthor, CommentAuthor)]
+## -------------------------------------------------------------------------
+p_first_posts_graph <- copy(p_first_period)
+p_first_posts_graph <- p_first_posts_graph[, .(Commentors = unlist(tstrsplit(CommentsItemCode, ", "))), by = "ItemCode"]
 
-#' 
-#' ### Graph 
-#' 
-## ---------------------------------------------------------------------------------------------------------------------------------
-g <- graph_from_data_frame(first_graph, directed = F)
+p_first_posts_graph <- merge(p_first_posts_graph, p_first_period, by.x = 'ItemCode', by.y = 'ItemCode')
 
-#' 
-## ---------------------------------------------------------------------------------------------------------------------------------
-g1=igraph::simplify(g, remove.multiple = T, remove.loops = T)
+p_first_posts_graph <- p_first_posts_graph[, .(PostItemCode = ItemCode, CommentItemCode = Commentors, PostAuthor = Author)]
 
-#' 
-## ---------------------------------------------------------------------------------------------------------------------------------
-plot( g1,vertex.label=NA,
-      edge.width = 1,
-      edge.arrow.width = 0.3,
-      edge.arrow.size = 0.5,
-      vertex.label.cex = 1,
-      asp = 0.35,
-      margin = -0.1)
+p_first_posts_graph <- merge(p_first_posts_graph, c_first_period, by.x = 'CommentItemCode', by.y = 'ItemCode')
+
+p_first_posts_graph <- p_first_posts_graph[, .(PostItemCode, CommentItemCode, PostAuthor, CommentAuthor = Author)]
+
+final_p_first_posts_graph <- p_first_posts_graph[, .(PostAuthor, CommentAuthor)]
+
+g_first_posts <- graph_from_data_frame(final_p_first_posts_graph, directed = F)
+
+g_first_posts <- igraph::simplify(g_first_posts, remove.multiple = T, remove.loops = T)
+
+nyms <- ifelse(V(g_first_posts)$name %in% contrib, "contributor", "other")
+
+g_first_posts <- set_vertex_attr(g_first_posts, "type", value = nyms)
+
+### Graph 
+
+coul  <- brewer.pal(3, "Set1") 
+my_color = coul[as.numeric(as.factor(V(g_first_posts)$type))]
+
+my_color[V(g_first_posts)$type=="other"] <- adjustcolor(my_color[V(g_first_posts)$type=="other"], alpha.f = 0.2)
+
+png(filename = 'images/first_period_graph.png')
+
+plot(g_first_posts,
+     vertex.label = NA,
+     vertex.color = my_color,
+     layout = layout_with_kk,
+     vertex.size=4,
+     edge.width=0.5,
+     edge.color="lightgrey",
+     vertex.frame.width = 0
+)
+
+dev.off()
 
 #' 
 #' 
 #' ## Second Period
 ## ---------------------------------------------------------------------------------------------------------------------------------
-second_graph <- copy(p_second_period)
-second_graph <- second_graph[, .(Commentors = unlist(tstrsplit(CommentsItemCode, ", "))), by = "ItemCode"]
-second_graph <- merge(second_graph, posts, by.x = 'ItemCode', by.y = 'ItemCode')
-second_graph <- second_graph[, .(PostItemCode = ItemCode, CommentItemCode = Commentors, PostAuthor = Author)]
-second_graph <- merge(second_graph, comments, by.x = 'CommentItemCode', by.y = 'ItemCode')
-second_graph <- second_graph[, .(PostItemCode, CommentItemCode, PostAuthor, CommentAuthor = Author)]
-second_graph <- second_graph[, .(PostAuthor, CommentAuthor)]
+p_second_posts_graph <- copy(p_second_period)
+p_second_posts_graph <- p_second_posts_graph[, .(Commentors = unlist(tstrsplit(CommentsItemCode, ", "))), by = "ItemCode"]
 
-#' 
-#' ### Graph 
-## ---------------------------------------------------------------------------------------------------------------------------------
-g2 <- graph_from_data_frame(second_graph, directed = F)
+p_second_posts_graph <- merge(p_second_posts_graph, p_second_period, by.x = 'ItemCode', by.y = 'ItemCode')
 
-#' 
-## ---------------------------------------------------------------------------------------------------------------------------------
-g2=igraph::simplify(g2, remove.multiple = T, remove.loops = T)
+p_second_posts_graph <- p_second_posts_graph[, .(PostItemCode = ItemCode, CommentItemCode = Commentors, PostAuthor = Author)]
 
+p_second_posts_graph <- merge(p_second_posts_graph, c_second_period, by.x = 'CommentItemCode', by.y = 'ItemCode')
+
+p_second_posts_graph <- p_second_posts_graph[, .(PostItemCode, CommentItemCode, PostAuthor, CommentAuthor = Author)]
+
+final_p_second_posts_graph <- p_second_posts_graph[, .(PostAuthor, CommentAuthor)]
+
+g_second_posts <- igraph::simplify(graph_from_data_frame(final_p_second_posts_graph, directed = F), remove.multiple = T, remove.loops = T)
+
+nyms <- ifelse(V(g_second_posts)$name %in% contrib, "contributor", "other")
+
+g_second_posts <- set_vertex_attr(g_second_posts, "type", value = nyms)
 #' 
 ## ---------------------------------------------------------------------------------------------------------------------------------
-plot( g2,vertex.label=NA,
-      edge.width = 1,
-      edge.arrow.width = 0.3,
-      edge.arrow.size = 0.5,
-      vertex.label.cex = 1,
-      asp = 0.35,
-      margin = -0.1)
+coul  <- brewer.pal(3, "Set1") 
+my_color = coul[as.numeric(as.factor(V(g_second_posts)$type))]
+
+my_color[V(g_second_posts)$type=="other"] <- adjustcolor(my_color[V(g_second_posts)$type=="other"], alpha.f = 0.2)
+
+png(filename = 'images/second_period_graph.png')
+
+plot(g_second_posts,
+     vertex.label = NA,
+     vertex.color = my_color,
+     layout = layout_with_kk,
+     vertex.size=4,
+     edge.width=0.5,
+     edge.color="lightgrey",
+     vertex.frame.width = 0
+)
+
+dev.off()
 
 #' 
 #' ## Third Period
 ## ---------------------------------------------------------------------------------------------------------------------------------
-third_graph <- copy(p_third_period)
-third_graph <- third_graph[, .(Commentors = unlist(tstrsplit(CommentsItemCode, ", "))), by = "ItemCode"]
-third_graph <- merge(third_graph, posts, by.x = 'ItemCode', by.y = 'ItemCode')
-third_graph <- third_graph[, .(PostItemCode = ItemCode, CommentItemCode = Commentors, PostAuthor = Author)]
-third_graph <- merge(third_graph, comments, by.x = 'CommentItemCode', by.y = 'ItemCode')
-third_graph <- third_graph[, .(PostItemCode, CommentItemCode, PostAuthor, CommentAuthor = Author)]
-third_graph <- third_graph[, .(PostAuthor, CommentAuthor)]
+p_third_posts_graph <- copy(p_third_period)
+p_third_posts_graph <- p_third_posts_graph[, .(Commentors = unlist(tstrsplit(CommentsItemCode, ", "))), by = "ItemCode"]
+
+p_third_posts_graph <- merge(p_third_posts_graph, p_third_period, by.x = 'ItemCode', by.y = 'ItemCode')
+
+p_third_posts_graph <- p_third_posts_graph[, .(PostItemCode = ItemCode, CommentItemCode = Commentors, PostAuthor = Author)]
+
+p_third_posts_graph <- merge(p_third_posts_graph, c_third_period, by.x = 'CommentItemCode', by.y = 'ItemCode')
+
+p_third_posts_graph <- p_third_posts_graph[, .(PostItemCode, CommentItemCode, PostAuthor, CommentAuthor = Author)]
+
+final_p_third_posts_graph <- p_third_posts_graph[, .(PostAuthor, CommentAuthor)]
+
+g_third_posts <- igraph::simplify(graph_from_data_frame(final_p_third_posts_graph, directed = F), remove.multiple = T, remove.loops = T)
+
+nyms <- ifelse(V(g_third_posts)$name %in% contrib, "contributor", "other")
+
+g_third_posts <- set_vertex_attr(g_third_posts, "type", value = nyms)
 
 #' 
-#' ### Graph 
 ## ---------------------------------------------------------------------------------------------------------------------------------
-g3 <- graph_from_data_frame(third_graph, directed = F)
 
-#' 
-## ---------------------------------------------------------------------------------------------------------------------------------
-g3=igraph::simplify(g3, remove.multiple = T, remove.loops = T)
+coul  <- brewer.pal(3, "Set1")
+my_color = coul[as.numeric(as.factor(V(g_third_posts)$type))]
 
-#' 
-## ---------------------------------------------------------------------------------------------------------------------------------
-plot( g3,vertex.label=NA,
-      edge.width = 1,
-      edge.arrow.width = 0.3,
-      edge.arrow.size = 0.5,
-      vertex.label.cex = 1,
-      asp = 0.35,
-      margin = -0.1)
+my_color[V(g_third_posts)$type=="other"] <- adjustcolor(my_color[V(g_third_posts)$type=="other"], alpha.f = 0.2)
+
+png(filename = 'images/third_period_graph.png')
+
+plot(g_third_posts,
+     vertex.label = NA,
+     vertex.color = my_color,
+     layout=layout_with_kk,
+     vertex.size=4,
+     edge.width=0.5,
+     edge.color="lightgrey",
+     vertex.frame.width = 0
+)
+
+dev.off()
 
 #' 
 #' ### Fourth Period
 ## ---------------------------------------------------------------------------------------------------------------------------------
-fourth_graph <- copy(p_fourth_period)
-fourth_graph <- fourth_graph[, .(Commentors = unlist(tstrsplit(CommentsItemCode, ", "))), by = "ItemCode"]
-fourth_graph <- merge(fourth_graph, posts, by.x = 'ItemCode', by.y = 'ItemCode')
-fourth_graph <- fourth_graph[, .(PostItemCode = ItemCode, CommentItemCode = Commentors, PostAuthor = Author)]
-fourth_graph <- merge(fourth_graph, comments, by.x = 'CommentItemCode', by.y = 'ItemCode')
-fourth_graph <- fourth_graph[, .(PostItemCode, CommentItemCode, PostAuthor, CommentAuthor = Author)]
-fourth_graph <- fourth_graph[, .(PostAuthor, CommentAuthor)]
+p_fourth_posts_graph <- copy(p_fourth_period)
+p_fourth_posts_graph <- p_fourth_posts_graph[, .(Commentors = unlist(tstrsplit(CommentsItemCode, ", "))), by = "ItemCode"]
 
+p_fourth_posts_graph <- merge(p_fourth_posts_graph, p_fourth_period, by.x = 'ItemCode', by.y = 'ItemCode')
+
+p_fourth_posts_graph <- p_fourth_posts_graph[, .(PostItemCode = ItemCode, CommentItemCode = Commentors, PostAuthor = Author)]
+
+p_fourth_posts_graph <- merge(p_fourth_posts_graph, c_fourth_period, by.x = 'CommentItemCode', by.y = 'ItemCode')
+
+p_fourth_posts_graph <- p_fourth_posts_graph[, .(PostItemCode, CommentItemCode, PostAuthor, CommentAuthor = Author)]
+
+final_p_fourth_posts_graph <- p_fourth_posts_graph[, .(PostAuthor, CommentAuthor)]
+
+g_fourth_posts <- igraph::simplify(graph_from_data_frame(final_p_fourth_posts_graph, directed = F), remove.multiple = T, remove.loops = T)
+
+nyms <- ifelse(V(g_fourth_posts)$name %in% contrib, "contributor", "other")
+
+g_fourth_posts <- set_vertex_attr(g_fourth_posts, "type", value = nyms)
 #' 
 #' ### Graph 
-## ---------------------------------------------------------------------------------------------------------------------------------
-g4 <- graph_from_data_frame(fourth_graph, directed = F)
+## -----------------------------------------------------------------------
+coul  <- brewer.pal(3, "Set1")
+my_color = coul[as.numeric(as.factor(V(g_fourth_posts)$type))]
 
-#' 
-## ---------------------------------------------------------------------------------------------------------------------------------
-g4=igraph::simplify(g4, remove.multiple = T, remove.loops = T)
+my_color[V(g_fourth_posts)$type=="other"] <- adjustcolor(my_color[V(g_fourth_posts)$type=="other"], alpha.f = 0.2)
 
-#' 
-## ---------------------------------------------------------------------------------------------------------------------------------
-plot( g4,vertex.label=NA,
-      edge.width = 1,
-      edge.arrow.width = 0.3,
-      edge.arrow.size = 0.5,
-      vertex.label.cex = 1,
-      asp = 0.35,
-      margin = -0.1)
+png(filename = 'images/fourth_period_graph.png')
+
+plot(g_fourth_posts,
+     vertex.label = NA,
+     vertex.color = my_color,
+     layout = layout_with_kk,
+     vertex.size=4,
+     edge.width=0.5,
+     edge.color="lightgrey",
+     vertex.frame.width = 0
+)
+
+dev.off()
 
 #' 
 #' ### Fifth Period
 ## ---------------------------------------------------------------------------------------------------------------------------------
-fifth_graph <- copy(p_fifth_period)
-fifth_graph <- fifth_graph[, .(Commentors = unlist(tstrsplit(CommentsItemCode, ", "))), by = "ItemCode"]
-fifth_graph <- merge(fifth_graph, posts, by.x = 'ItemCode', by.y = 'ItemCode')
-fifth_graph <- fifth_graph[, .(PostItemCode = ItemCode, CommentItemCode = Commentors, PostAuthor = Author)]
-fifth_graph <- merge(fifth_graph, comments, by.x = 'CommentItemCode', by.y = 'ItemCode')
-fifth_graph <- fifth_graph[, .(PostItemCode, CommentItemCode, PostAuthor, CommentAuthor = Author)]
-fifth_graph <- fifth_graph[, .(PostAuthor, CommentAuthor)]
+p_fifth_posts_graph <- copy(p_fifth_period)
+
+p_fifth_posts_graph[1, CommentsItemCode := NA]
+
+p_fifth_posts_graph <- p_fifth_posts_graph[, .(Commentors = unlist(tstrsplit(CommentsItemCode, ", "))), by = "ItemCode"]
+
+p_fifth_posts_graph <- merge(p_fifth_posts_graph, p_fifth_period, by.x = 'ItemCode', by.y = 'ItemCode')
+
+p_fifth_posts_graph <- p_fifth_posts_graph[, .(PostItemCode = ItemCode, CommentItemCode = Commentors, PostAuthor = Author)]
+
+p_fifth_posts_graph <- merge(p_fifth_posts_graph, c_fifth_period, by.x = 'CommentItemCode', by.y = 'ItemCode')
+
+p_fifth_posts_graph <- p_fifth_posts_graph[, .(PostItemCode, CommentItemCode, PostAuthor, CommentAuthor = Author)]
+
+final_p_fifth_posts_graph <- p_fifth_posts_graph[, .(PostAuthor, CommentAuthor)]
+
+g_fifth_posts <- igraph::simplify(graph_from_data_frame(final_p_fifth_posts_graph, directed = F), remove.multiple = T, remove.loops = T)
+
+nyms <- ifelse(V(g_fifth_posts)$name %in% contrib, "contributor", "other")
+
+g_fifth_posts <- set_vertex_attr(g_fifth_posts, "type", value = nyms)
 
 #' 
 #' ### Graph 
 ## ---------------------------------------------------------------------------------------------------------------------------------
-g5 <- graph_from_data_frame(fifth_graph, directed = F)
+coul  <- brewer.pal(3, "Set1")
+my_color = coul[as.numeric(as.factor(V(g_fifth_posts)$type))]
 
-#' 
-## ---------------------------------------------------------------------------------------------------------------------------------
-g5=igraph::simplify(g5, remove.multiple = T, remove.loops = T)
+my_color[V(g_fifth_posts)$type=="other"] <- adjustcolor(my_color[V(g_fifth_posts)$type=="other"], alpha.f = 0.2)
 
-#' 
-## ---------------------------------------------------------------------------------------------------------------------------------
-plot( g5,vertex.label=NA,
-      edge.width = 1,
-      edge.arrow.width = 0.3,
-      edge.arrow.size = 0.5,
-      vertex.label.cex = 1,
-      asp = 0.35,
-      margin = -0.1)
+png(filename = 'images/fifth_period_graph.png')
 
+plot(g_fifth_posts,
+     vertex.label = NA,
+     vertex.color = my_color,
+     layout = layout_with_kk,
+     vertex.size=4,
+     edge.width=0.5,
+     edge.color="lightgrey",
+     vertex.frame.width = 0
+)
+
+dev.off()
