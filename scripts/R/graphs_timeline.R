633c8e856de1b2d83930b756a7a33dbfd2c68d27
FiliTol <tolinfilippo@gmail.com>
2023-11-08 23:29:23 +0100

    Added attributes on stacked amount and categorical stacked amount to the first graph

--- scripts/R/graphs_timeline.R
+++ scripts/R/graphs_timeline.R
@@ -35,7 +35,14 @@ contrib <- c("k00b",
 #' Network Analysis
 #' 
 #' ## First Period
-## -------------------------------------------------------------------------
+
+## Retrieve stacked amounts in the period for every user
+p_author_first_stacked <- p_first_period[, .(Sats = sum(Sats)), by = .(Author)]
+c_author_first_stacked <- c_first_period[, .(Sats = sum(Sats)), by = .(Author)]
+
+author_first_stacked <- rbindlist(list(p_author_first_stacked, c_author_first_stacked))[, lapply(.SD, sum, na.rm = TRUE), by = Author]
+
+# Create table for graph generation 
 p_first_posts_graph <- copy(p_first_period)
 p_first_posts_graph <- p_first_posts_graph[, .(Commentors = unlist(tstrsplit(CommentsItemCode, ", "))), by = "ItemCode"]
 
@@ -49,27 +56,44 @@ p_first_posts_graph <- p_first_posts_graph[, .(PostItemCode, CommentItemCode, Po
 
 final_p_first_posts_graph <- p_first_posts_graph[, .(PostAuthor, CommentAuthor)]
 
+# Computing and inserting weights as the number of interactions between users
+final_p_first_posts_graph <- final_p_first_posts_graph[, .(weight = .N), by = .(Author1 = pmax(PostAuthor, CommentAuthor), Author2 = pmin(PostAuthor, CommentAuthor))]
+
 g_first_posts <- graph_from_data_frame(final_p_first_posts_graph, directed = F)
 
 g_first_posts <- igraph::simplify(g_first_posts, remove.multiple = T, remove.loops = T)
 
 nyms <- ifelse(V(g_first_posts)$name %in% contrib, "contributor", "other")
 
+# Add user type as attribute
 g_first_posts <- set_vertex_attr(g_first_posts, "type", value = nyms)
+
+# Add stacked amount as attribute
+V(g_first_posts)$Sats <- author_first_stacked$Sats[match(V(g_first_posts)$name, author_first_stacked$Author)]
+
+# Add categorical amount by dividing the users according to the quartiles of Sats distribution
+quartiles <- quantile(V(g_first_posts)$Sats, probs = c(0, 0.25, 0.5, 0.75, 1), na.rm = TRUE)
+
+V(g_first_posts)$catSats <- cut(author_first_stacked$Sats, breaks = quartiles, labels = c("Q1", "Q2", "Q3", "Q4"), include.lowest = TRUE)[match(V(g_first_posts)$name, author_first_stacked$Author)]
 
 ### Graph 
 
-coul  <- brewer.pal(3, "Set1") 
-my_color = coul[as.numeric(as.factor(V(g_first_posts)$type))]
+coul  <- brewer.pal(3, "Reds") 
 
-my_color[V(g_first_posts)$type=="other"] <- adjustcolor(my_color[V(g_first_posts)$type=="other"], alpha.f = 0.2)
+my_color = coul[as.numeric(as.factor(V(g_first_posts)$catSats))]
+
+# Set alpha of vertex based on the stacked amount category
+my_color[V(g_first_posts)$catSats=="Q1"] <- adjustcolor(my_color[V(g_first_posts)$catSats=="Q1"], alpha.f = 0.25)
+my_color[V(g_first_posts)$catSats=="Q2"] <- adjustcolor(my_color[V(g_first_posts)$catSats=="Q2"], alpha.f = 0.50)
+my_color[V(g_first_posts)$catSats=="Q3"] <- adjustcolor(my_color[V(g_first_posts)$catSats=="Q3"], alpha.f = 0.75)
+my_color[V(g_first_posts)$catSats=="Q4"] <- adjustcolor(my_color[V(g_first_posts)$catSats=="Q4"], alpha.f = 1)
 
 png(filename = 'images/first_period_graph.png')
 
 plot(g_first_posts,
      vertex.label = NA,
      vertex.color = my_color,
-     layout = layout_with_kk,
+     layout = layout_with_fr,
      vertex.size=4,
      edge.width=0.5,
      edge.color="lightgrey",
@@ -95,6 +119,8 @@ p_second_posts_graph <- p_second_posts_graph[, .(PostItemCode, CommentItemCode, 
 
 final_p_second_posts_graph <- p_second_posts_graph[, .(PostAuthor, CommentAuthor)]
 
+final_p_second_posts_graph <- final_p_second_posts_graph[, .(weight = .N), by = .(Author1 = pmax(PostAuthor, CommentAuthor), Author2 = pmin(PostAuthor, CommentAuthor))]
+
 g_second_posts <- igraph::simplify(graph_from_data_frame(final_p_second_posts_graph, directed = F), remove.multiple = T, remove.loops = T)
 
 nyms <- ifelse(V(g_second_posts)$name %in% contrib, "contributor", "other")
@@ -112,7 +138,7 @@ png(filename = 'images/second_period_graph.png')
 plot(g_second_posts,
      vertex.label = NA,
      vertex.color = my_color,
-     layout = layout_with_kk,
+     layout = layout_with_fr,
      vertex.size=4,
      edge.width=0.5,
      edge.color="lightgrey",
@@ -137,6 +163,8 @@ p_third_posts_graph <- p_third_posts_graph[, .(PostItemCode, CommentItemCode, Po
 
 final_p_third_posts_graph <- p_third_posts_graph[, .(PostAuthor, CommentAuthor)]
 
+final_p_third_posts_graph <- final_p_third_posts_graph[, .(weight = .N), by = .(Author1 = pmax(PostAuthor, CommentAuthor), Author2 = pmin(PostAuthor, CommentAuthor))]
+
 g_third_posts <- igraph::simplify(graph_from_data_frame(final_p_third_posts_graph, directed = F), remove.multiple = T, remove.loops = T)
 
 nyms <- ifelse(V(g_third_posts)$name %in% contrib, "contributor", "other")
@@ -156,7 +184,7 @@ png(filename = 'images/third_period_graph.png')
 plot(g_third_posts,
      vertex.label = NA,
      vertex.color = my_color,
-     layout=layout_with_kk,
+     layout=layout_with_fr,
      vertex.size=4,
      edge.width=0.5,
      edge.color="lightgrey",
@@ -181,6 +209,8 @@ p_fourth_posts_graph <- p_fourth_posts_graph[, .(PostItemCode, CommentItemCode, 
 
 final_p_fourth_posts_graph <- p_fourth_posts_graph[, .(PostAuthor, CommentAuthor)]
 
+final_p_fourth_posts_graph <- final_p_fourth_posts_graph[, .(weight = .N), by = .(Author1 = pmax(PostAuthor, CommentAuthor), Author2 = pmin(PostAuthor, CommentAuthor))]
+
 g_fourth_posts <- igraph::simplify(graph_from_data_frame(final_p_fourth_posts_graph, directed = F), remove.multiple = T, remove.loops = T)
 
 nyms <- ifelse(V(g_fourth_posts)$name %in% contrib, "contributor", "other")
@@ -199,7 +229,7 @@ png(filename = 'images/fourth_period_graph.png')
 plot(g_fourth_posts,
      vertex.label = NA,
      vertex.color = my_color,
-     layout = layout_with_kk,
+     layout = layout_with_fr,
      vertex.size=4,
      edge.width=0.5,
      edge.color="lightgrey",
@@ -227,6 +257,8 @@ p_fifth_posts_graph <- p_fifth_posts_graph[, .(PostItemCode, CommentItemCode, Po
 
 final_p_fifth_posts_graph <- p_fifth_posts_graph[, .(PostAuthor, CommentAuthor)]
 
+final_p_fifth_posts_graph <- final_p_fifth_posts_graph[, .(weight = .N), by = .(Author1 = pmax(PostAuthor, CommentAuthor), Author2 = pmin(PostAuthor, CommentAuthor))]
+
 g_fifth_posts <- igraph::simplify(graph_from_data_frame(final_p_fifth_posts_graph, directed = F), remove.multiple = T, remove.loops = T)
 
 nyms <- ifelse(V(g_fifth_posts)$name %in% contrib, "contributor", "other")
@@ -246,7 +278,7 @@ png(filename = 'images/fifth_period_graph.png')
 plot(g_fifth_posts,
      vertex.label = NA,
      vertex.color = my_color,
-     layout = layout_with_kk,
+     layout = layout_with_fr,
      vertex.size=4,
      edge.width=0.5,
      edge.color="lightgrey",
