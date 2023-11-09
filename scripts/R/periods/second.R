4e7c9c68a8d1892ab2ba611189dd2b89c8579872
FiliTol <tolinfilippo@gmail.com>
2023-11-09 20:43:53 +0100

    Added analysis script about the second period

--- scripts/R/periods/second.R
+++ scripts/R/periods/second.R
@@ -11,14 +11,14 @@ library(gmp)
 library(slam)
 
 
-#' # Load RDS files
+# Load RDS files
 comments <- readRDS(file = 'RDS_files/comments')
 posts <- readRDS(file = 'RDS_files/posts')
 users <- readRDS(file = 'RDS_files/users')
 
-p_first_period <- readRDS(file = 'RDS_files/p_first_period')
+p_second_period <- readRDS(file = 'RDS_files/p_second_period')
 
-c_first_period <- readRDS(file = 'RDS_files/c_first_period')
+c_second_period <- readRDS(file = 'RDS_files/c_second_period')
 
 contrib <- c("k00b",
              "kr",
@@ -32,60 +32,60 @@ contrib <- c("k00b",
 # Network Analysis
 
 ## Retrieve stacked amounts in the period for every user
-p_author_first_stacked <- p_first_period[, .(Sats = sum(Sats)), by = .(Author)]
-c_author_first_stacked <- c_first_period[, .(Sats = sum(Sats)), by = .(Author)]
+p_author_second_stacked <- p_second_period[, .(Sats = sum(Sats)), by = .(Author)]
+c_author_second_stacked <- c_second_period[, .(Sats = sum(Sats)), by = .(Author)]
 
-author_first_stacked <- rbindlist(list(p_author_first_stacked, c_author_first_stacked))[, lapply(.SD, sum, na.rm = TRUE), by = Author]
+author_second_stacked <- rbindlist(list(p_author_second_stacked, c_author_second_stacked))[, lapply(.SD, sum, na.rm = TRUE), by = Author]
 
 # Create table for graph generation 
-p_first_posts_graph <- copy(p_first_period)
-p_first_posts_graph <- p_first_posts_graph[, .(Commentors = unlist(tstrsplit(CommentsItemCode, ", "))), by = "ItemCode"]
+p_second_posts_graph <- copy(p_second_period)
+p_second_posts_graph <- p_second_posts_graph[, .(Commentors = unlist(tstrsplit(CommentsItemCode, ", "))), by = "ItemCode"]
 
-p_first_posts_graph <- merge(p_first_posts_graph, p_first_period, by.x = 'ItemCode', by.y = 'ItemCode')
+p_second_posts_graph <- merge(p_second_posts_graph, p_second_period, by.x = 'ItemCode', by.y = 'ItemCode')
 
-p_first_posts_graph <- p_first_posts_graph[, .(PostItemCode = ItemCode, CommentItemCode = Commentors, PostAuthor = Author)]
+p_second_posts_graph <- p_second_posts_graph[, .(PostItemCode = ItemCode, CommentItemCode = Commentors, PostAuthor = Author)]
 
-p_first_posts_graph <- merge(p_first_posts_graph, c_first_period, by.x = 'CommentItemCode', by.y = 'ItemCode')
+p_second_posts_graph <- merge(p_second_posts_graph, c_second_period, by.x = 'CommentItemCode', by.y = 'ItemCode')
 
-p_first_posts_graph <- p_first_posts_graph[, .(PostItemCode, CommentItemCode, PostAuthor, CommentAuthor = Author)]
+p_second_posts_graph <- p_second_posts_graph[, .(PostItemCode, CommentItemCode, PostAuthor, CommentAuthor = Author)]
 
-final_p_first_posts_graph <- p_first_posts_graph[, .(PostAuthor, CommentAuthor)]
+final_p_second_posts_graph <- p_second_posts_graph[, .(PostAuthor, CommentAuthor)]
 
 # Computing and inserting weights as the number of interactions between users
-final_p_first_posts_graph <- final_p_first_posts_graph[, .(weight = .N), by = .(Author1 = pmax(PostAuthor, CommentAuthor), Author2 = pmin(PostAuthor, CommentAuthor))]
+final_p_second_posts_graph <- final_p_second_posts_graph[, .(weight = .N), by = .(Author1 = pmax(PostAuthor, CommentAuthor), Author2 = pmin(PostAuthor, CommentAuthor))]
 
-g_first_posts <- graph_from_data_frame(final_p_first_posts_graph, directed = F)
+g_second_posts <- graph_from_data_frame(final_p_second_posts_graph, directed = F)
 
-g_first_posts <- igraph::simplify(g_first_posts, remove.multiple = T, remove.loops = T)
+g_second_posts <- igraph::simplify(g_second_posts, remove.multiple = T, remove.loops = T)
 
-nyms <- ifelse(V(g_first_posts)$name %in% contrib, "contributor", "other")
+nyms <- ifelse(V(g_second_posts)$name %in% contrib, "contributor", "other")
 
 # Add user type as attribute
-g_first_posts <- set_vertex_attr(g_first_posts, "type", value = nyms)
+g_second_posts <- set_vertex_attr(g_second_posts, "type", value = nyms)
 
 # Add stacked amount as attribute
-V(g_first_posts)$Sats <- author_first_stacked$Sats[match(V(g_first_posts)$name, author_first_stacked$Author)]
+V(g_second_posts)$Sats <- author_second_stacked$Sats[match(V(g_second_posts)$name, author_second_stacked$Author)]
 
 # Add categorical amount by dividing the users according to the quartiles of Sats distribution
-quartiles <- quantile(V(g_first_posts)$Sats, probs = c(0, 0.25, 0.5, 0.75, 1), na.rm = TRUE)
+quartiles <- quantile(V(g_second_posts)$Sats, probs = c(0, 0.25, 0.5, 0.75, 1), na.rm = TRUE)
 
-V(g_first_posts)$catSats <- cut(author_first_stacked$Sats, breaks = quartiles, labels = c("Q1", "Q2", "Q3", "Q4"), include.lowest = TRUE)[match(V(g_first_posts)$name, author_first_stacked$Author)]
+V(g_second_posts)$catSats <- cut(author_second_stacked$Sats, breaks = quartiles, labels = c("Q1", "Q2", "Q3", "Q4"), include.lowest = TRUE)[match(V(g_second_posts)$name, author_second_stacked$Author)]
 
 ### Graph 
 
 coul  <- brewer.pal(4, "Set1") 
 
-my_color = coul[as.numeric(as.factor(V(g_first_posts)$catSats))]
+my_color = coul[as.numeric(as.factor(V(g_second_posts)$catSats))]
 
 # # Set alpha of vertex based on the stacked amount category
-# my_color[V(g_first_posts)$catSats=="Q1"] <- adjustcolor(my_color[V(g_first_posts)$catSats=="Q1"], alpha.f = 0.25)
-# my_color[V(g_first_posts)$catSats=="Q2"] <- adjustcolor(my_color[V(g_first_posts)$catSats=="Q2"], alpha.f = 0.50)
-# my_color[V(g_first_posts)$catSats=="Q3"] <- adjustcolor(my_color[V(g_first_posts)$catSats=="Q3"], alpha.f = 0.75)
-# my_color[V(g_first_posts)$catSats=="Q4"] <- adjustcolor(my_color[V(g_first_posts)$catSats=="Q4"], alpha.f = 1)
+# my_color[V(g_second_posts)$catSats=="Q1"] <- adjustcolor(my_color[V(g_second_posts)$catSats=="Q1"], alpha.f = 0.25)
+# my_color[V(g_second_posts)$catSats=="Q2"] <- adjustcolor(my_color[V(g_second_posts)$catSats=="Q2"], alpha.f = 0.50)
+# my_color[V(g_second_posts)$catSats=="Q3"] <- adjustcolor(my_color[V(g_second_posts)$catSats=="Q3"], alpha.f = 0.75)
+# my_color[V(g_second_posts)$catSats=="Q4"] <- adjustcolor(my_color[V(g_second_posts)$catSats=="Q4"], alpha.f = 1)
 
-png(filename = 'images/first/first_period_graph.png')
+png(filename = 'images/second/second_period_graph.png')
 
-plot(g_first_posts,
+plot(g_second_posts,
      vertex.label = NA,
      vertex.color = my_color,
      layout = layout_with_fr,
@@ -96,7 +96,7 @@ plot(g_first_posts,
 )
 
 legend('topright',
-       legend = unique(V(g_first_posts)$catSats),
+       legend = unique(V(g_second_posts)$catSats),
        fill = unique(my_color),
        title = "Quartiles",
        bty = "n",
@@ -109,10 +109,10 @@ dev.off()
 ## Analysis of graph parameters and stats
 
 ### Degree of nodes and degree distribution
-degree_tab <- data.table(author = V(g_first_posts)$name,
+degree_tab <- data.table(author = V(g_second_posts)$name,
                          degr = degree(
-                           g_first_posts, 
-                           V(g_first_posts)$name,
+                           g_second_posts, 
+                           V(g_second_posts)$name,
                            loops = F
                          )
 ) %>%
@@ -122,16 +122,13 @@ degree_tab %>%
   head(10)
 
 degree_tab %>%
-  summarise(mean = mean(degr))
-
-degree_tab %>%
-  summarise(med = median(degr))
+  summarise(mean = mean(degr), median = median(degr) )
 
 # Plot degree distribution with log(x) scale
 ggplot(data = degree_tab)+
   geom_histogram(aes(x = degr))
 
-ggsave('images/first/degree_distribution.png')
+ggsave('images/second/degree_distribution.png')
 
 degree_tab %>%
   filter(author %in% contrib)
@@ -139,16 +136,16 @@ degree_tab %>%
 ##------------------------------------------------------------------------------
 ## Components
 
-component_distribution(g_first_posts)
+component_distribution(g_second_posts)
 
-largest_component(g_first_posts)
+largest_component(g_second_posts)
 
-count_components(g_first_posts)
+count_components(g_second_posts)
 
-components(g_first_posts)$csize
+components(g_second_posts)$csize
 
 # Visualize only the big component
-plot(decompose(g_first_posts)[[1]],
+plot(decompose(g_second_posts)[[1]],
      vertex.label = NA,
      vertex.color = my_color,
      layout = layout_with_fr,
@@ -159,7 +156,7 @@ plot(decompose(g_first_posts)[[1]],
 )
 
 legend('topright',
-       legend = unique(V(g_first_posts)$catSats),
+       legend = unique(V(g_second_posts)$catSats),
        fill = unique(my_color),
        title = "Quartiles",
        bty = "n",
@@ -173,20 +170,20 @@ legend('topright',
 ## of weights. Therefore diameter=12 is the sum of the weights along the edges 
 
 ### Diameter
-diameter(g_first_posts, directed = F, unconnected = T)
+diameter(g_second_posts, directed = F, unconnected = T)
 
 #### Diameter complete path
-get_diameter(g_first_posts, directed = F, unconnected = T)
+get_diameter(g_second_posts, directed = F, unconnected = T)
 
 #### Diameter vertices and distance
-farthest_vertices(g_first_posts, directed = F, unconnected = T)
+farthest_vertices(g_second_posts, directed = F, unconnected = T)
 
-mean_distance(g_first_posts, directed = F, unconnected = T)
+mean_distance(g_second_posts, directed = F, unconnected = T)
 
-#View(distances(g_first_posts))
+#View(distances(g_second_posts))
 
 # Takes upper triangle of the matrix (excluding diagonal entries)
-distances_vector <- data.frame(distances = as.vector(distances(g_first_posts))[upper.tri(distances(g_first_posts))])
+distances_vector <- data.frame(distances = as.vector(distances(g_second_posts))[upper.tri(distances(g_second_posts))])
 
 # Eliminate inf values, that are vertex couples not connected by any path
 distances_vector <- data.frame(distances = distances_vector[is.finite(rowSums(distances_vector)),])
@@ -201,11 +198,11 @@ distances_vector %>%
 ggplot(data = distances_vector)+
   geom_bar(aes(x = distances))
 
-ggsave('images/first/degree_of_separation.png')
+ggsave('images/second/degree_of_separation.png')
 ## -----------------------------------------------------------------------------
 ## Clustering and partitioning
 
-betweenness(g_first_posts)
+betweenness(g_second_posts)
 
 ### Community detection algorithms aim to find the division of a network that maximizes its modularity
 ### Modularity ranges from -1 to 1:
@@ -217,12 +214,12 @@ betweenness(g_first_posts)
 ## Edge betweenness clustering ##
 #################################
 
-CommunityBetweenness <- cluster_edge_betweenness(g_first_posts)
+CommunityBetweenness <- cluster_edge_betweenness(g_second_posts)
 
 print(CommunityBetweenness)
 
-# Clustering isolated 36 groups
-# Mudularity equal to 0.05 indicates a 'neutral' community structure, meaning
+# Clustering isolated 38 groups
+# Modularity equal to 0.01 indicates a 'neutral' community structure, meaning
 # that the network is barely divided into communities 
 
 
@@ -230,12 +227,12 @@ print(CommunityBetweenness)
 ## Louvian clustering ##
 ########################
 
-CommunityLouvian <- cluster_louvain(g_first_posts)
+CommunityLouvian <- cluster_louvain(g_second_posts)
 
 print(CommunityLouvian)
 
-# Clustering isolated 17
-# Mudularity equal to 0.16 indicates a relevant community structure, meaning
+# Clustering isolated 15
+# Modularity equal to 0.21 indicates a considerably relevant community structure, meaning
 # that the network is divided into communities 
 
 
@@ -243,12 +240,12 @@ print(CommunityLouvian)
 ## Walktrap clustering ##
 #########################
 
-CommunityWalktrap <- cluster_walktrap(g_first_posts)
+CommunityWalktrap <- cluster_walktrap(g_second_posts)
 
 print(CommunityWalktrap)
 
-# Clustering isolated 169
-# Mudularity equal to 0.083 indicates a 'neutral' community structure, meaning
+# Clustering isolated 539
+# Modularity equal to 0.12 indicates a relevant community structure, meaning
 # that the network is barely divided into communities
 
 
@@ -256,12 +253,12 @@ print(CommunityWalktrap)
 ## Fast Greedy clustering ##
 ############################
 
-CommunityFastGreedy <- cluster_fast_greedy(g_first_posts)
+CommunityFastGreedy <- cluster_fast_greedy(g_second_posts)
 
 print(CommunityFastGreedy)
 
-# Clustering isolated 19
-# Mudularity equal to 0.15 indicates a relevant community structure, meaning
+# Clustering isolated 17
+# Modularity equal to 0.21 indicates a considerably relevant community structure, meaning
 # that the network is divided into communities 
 
 
@@ -269,18 +266,18 @@ print(CommunityFastGreedy)
 ## Leading Eigenvector clustering ##
 ####################################
 
-CommunityLeadingEigenvector <- cluster_leading_eigen(g_first_posts)
+CommunityLeadingEigenvector <- cluster_leading_eigen(g_second_posts)
 
 print(CommunityLeadingEigenvector)
 
-# Clustering isolated 39
-# Mudularity equal to 0.12 indicates a relevant community structure, meaning
+# Clustering isolated 111
+# Mudularity equal to 0.16 indicates a relevant community structure, meaning
 # that the network is divided into communities 
 
-plot_dendrogram(CommunityBetweenness)
-plot_dendrogram(CommunityFastGreedy)
-plot_dendrogram(CommunityLeadingEigenvector)
-plot_dendrogram(CommunityWalktrap)
+# plot_dendrogram(CommunityBetweenness)
+# plot_dendrogram(CommunityFastGreedy)
+# plot_dendrogram(CommunityLeadingEigenvector)
+# plot_dendrogram(CommunityWalktrap)
 
 ## -----------------------------------------------------------------------------
 ## Comparing community structures
