eec79c31ac31571c7a684482a00b3ee247f4f72e
FiliTol <tolinfilippo@gmail.com>
2023-11-12 16:02:04 +0100

    Added attributes creation for directed weighted graph

--- scripts/R/test.R
+++ scripts/R/test.R
@@ -15,8 +15,24 @@ comments <- readRDS(file = 'RDS_files/comments')
 posts <- readRDS(file = 'RDS_files/posts')
 users <- readRDS(file = 'RDS_files/users')
 
-# Let's keep only the variables that are present in both posts and comments
+contrib <- c("k00b",
+             "kr",
+             "ekzyis",
+             "WeAreAllSatoshi",
+             "rleed",
+             "bitcoinplebdev",
+             "benthecarman"
+)
 
+
+## Retrieve stacked amounts in the period for every user
+posts_author_stacked <- posts[, .(Sats = sum(Sats)), by = .(Author)]
+comments_author_stacked <- comments[, .(Sats = sum(Sats)), by = .(Author)]
+
+author_stacked <- rbindlist(list(posts_author_stacked, comments_author_stacked))[, lapply(.SD, sum, na.rm = TRUE), by = Author]
+
+
+# Let's keep only the variables that are present in both posts and comments
 clean_posts <- subset(posts, select = c(ItemCode, Sats, Author, Timestamp, CommentsItemCode))
 clean_comments <- subset(comments, select = c(ItemCode, Sats, Author, Timestamp, CommentsItemCode))
 
@@ -45,5 +61,27 @@ final_graph <- final_graph[, .(weight = .N), by = .(Author2, Author1)]
 
 g <- graph_from_data_frame(final_graph, directed = T)
 
-plot(g)
+# Assign user type: contributor is a forum administrator and github mantainer
+nyms <- ifelse(V(g)$name %in% contrib, "contributor", "other")
+g <- set_vertex_attr(g, "type", value = nyms)
+
+# Add stacked amount as attribute
+V(g)$Sats <- author_stacked$Sats[match(V(g)$name, author_stacked$Author)]
+
+# Add categorical amount by dividing the users according to the quartiles of Sats distribution
+quartiles <- quantile(V(g)$Sats, probs = c(0, 0.25, 0.5, 0.75, 1), na.rm = TRUE)
+
+V(g)$catSats <- cut(author_stacked$Sats, breaks = quartiles, labels = c("Q1", "Q2", "Q3", "Q4"), include.lowest = TRUE)[match(V(g)$name, author_stacked$Author)]
+
+
+
+
+
+
+
+
+
+
+
+
 
